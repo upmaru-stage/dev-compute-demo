a396c00c1ba9e13f2528365e3f63bfe2b29a058f
Zack Siri <me@zacksiri.dev>
2024-04-26 16:25:46 +0700

    Try develop network module

--- networkings.tf
+++ networkings.tf
@@ -2,8 +2,10 @@
 
 variable "identifier" {}
 module "aws_networking_xandar_uzk" {
-  source  = "upmaru/instellar/aws//modules/network"
-  version = "0.8.5"
+  # source  = "upmaru/instellar/aws//modules/network"
+  source = "github.com/upmaru/terraform-aws-instellar//modules/network?ref=develop"
+
+  # version = "0.8.5"
 
   blueprint           = var.identifier
   identifier          = var.identifier
