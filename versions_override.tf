e1e7fe4e51ecfc7998bfe57f9d4998507f64b0ee
Zack Siri <me@zacksiri.dev>
2024-04-30 10:44:03 +0700

    Add versions override

---
+++ versions_override.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+
+  cloud {
+    organization = "upmaru-stage"
+
+    workspaces {
+      name = "dev-compute-demo"
+    }
+  }
+}
