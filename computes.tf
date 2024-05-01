0e280ab2835fe4226ea64cd57f66d3b80f0bee93
Zack Siri <me@zacksiri.dev>
2024-05-01 14:39:03 +0700

    Enable public port

--- computes.tf
+++ computes.tf
@@ -21,7 +21,7 @@ module "aws_compute_xandar_dga" {
   node_detail_revision = 2
   node_size            = "t3a.medium"
   public_subnet_ids    = module.aws_networking_xandar_uzk.public_subnet_ids
-  publicly_accessible  = false
+  publicly_accessible  = true
   region               = var.aws_region
   ssh_keys             = ["zack-studio", "zack-mbp"]
   ssm                  = false
