7cc2997d351c3c9cb443a3d8f125b07fd5323783
Zack Siri <me@zacksiri.dev>
2024-07-15 13:56:09 +0700

    Put back tunnel

--- tunnels.tf
+++ tunnels.tf
@@ -1,20 +1,20 @@
-# variable "cloudflare_api_token" {}
-# variable "cloudflare_account_id" {}
+variable "cloudflare_api_token" {}
+variable "cloudflare_account_id" {}
 
-# provider "cloudflare" {
-#   api_token = var.cloudflare_api_token
-# }
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
 
-# module "cloudflare_tunnel_xandar_dga" {
-#   source  = "upmaru/instellar/cloudflare"
-#   version = "0.1.1"
+module "cloudflare_tunnel_xandar_dga" {
+  source  = "upmaru/instellar/cloudflare"
+  version = "0.1.1"
 
-#   bastion_access = module.aws_compute_xandar_dga.bastion_access
-#   blueprint      = var.identifier
+  bastion_access = module.aws_compute_xandar_dga.bastion_access
+  blueprint      = var.identifier
 
-#   identifier            = module.aws_compute_xandar_dga.identifier
-#   cloudflare_account_id = var.cloudflare_account_id
+  identifier            = module.aws_compute_xandar_dga.identifier
+  cloudflare_account_id = var.cloudflare_account_id
 
-#   zone_name = "opsmaru.net"
-#   emails    = ["zack@upmaru.com"]
-# }
+  zone_name = "opsmaru.net"
+  emails    = ["zack@upmaru.com"]
+}
